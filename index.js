44ef45d69ffe26173f6cc6c49fd73a9b5dc95de4
Samuel <lee.eunseong7856@gmail.com>
2022-10-30 17:30:59 -0500

    commit tree 00f49428b2bb035c6e7b4a109de9120e97120fbb

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    switch (blocks) {
+        case 43 :
+            return 1;
+        case 50 : 
+            return 8;
+        case 34 :
+            return 8;
+    }
+}
+
+function distanceFromHqInFeet(feet) {
+    switch (feet) {
+        case 43 :
+            return 264
+        case 50 :
+            return 2112;
+        case 34 :
+            return 2112;
+    }
+}
+
+
+function distanceTravelledInFeet(ft1, ft2) {
+    switch (ft1, ft2) {
+        case 43, 48 :
+            return 1320;
+        case 50, 60 :
+            return 2640;
+        case 34, 28 :
+            return 1584;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    switch(start, destination) {
+        case 43, 44 :
+            return 0;
+        case 34, 32 :
+            return 2.56;
+        case 50, 58 :
+            return 25;
+        case 34, 24 :
+            return 'cannot travel that far';
+    }
+}
